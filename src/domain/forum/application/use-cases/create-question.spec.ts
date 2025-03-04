641213de2871d4361e6cf394f498e4bd2222f1fb
joschonarth <joschonarth@gmail.com>
2025-03-04 10:50:35 -0300

    feat: implement create question use case

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,19 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (_question: Question) => {},
+}
+
+test('create a question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'New question',
+    content: 'Question content',
+  })
+
+  expect(question.id).toBeTruthy()
+})
